eadefaf7e4f6268cf8f42946b7ea7f663f6712f5
David Moreira <dmoreira@gopro.com>
2021-05-10 11:57:36 +0000

    some update.

--- test.nix
+++ test.nix
@@ -1,21 +1,30 @@
 with import <nixpkgs> {
 };
-stdenv.mkDerivation {
-  name = "gpdev_client";
-  version = "0.0";
-  src = ../test;
+{ ... }:
+let
+  pkgs = (import <nixpkgs> {});
+  
+in {
+  hello = stdenv.mkDerivation {
+    name = "gpdev_client";
+    version = "0.0";
+    src = ../test;
 
-  buildInputs = [
-    boost
-    cmake
-  ];
+    buildInputs = [
+      boost
+      cmake
+    ];
 
-  configurePhase = ''
-    true
-  '';
+    configurePhase = ''
+      echo configure phase done
+    '';
 
-  buildPhase = ''
-    true
-    false
-  '';
+    buildPhase = ''
+      echo build phase done
+    '';
+    installPhase = ''
+      echo install phase done
+    '';
+  };
+
 }
